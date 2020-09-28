556a126dfbfae8b218ccaaff802ad2a965fe5c43
Zulfikri Ramadhan Yoga <50226344+ctrlbzul5@users.noreply.github.com>
2020-09-28 22:15:51 +0700

    Update guessThreeTimes.cpp

--- guessThreeTimes.cpp
+++ guessThreeTimes.cpp
@@ -1,32 +1,26 @@
 #include <iostream>
 using namespace std;
 
-void lineBar() {
-	cout << "====================================\n";
-}
-
 int main() {
 	int a = 4, b = 6, result, guess;
-	int wrong = 0;
+	int wrong = 0; // chance
     result = a + b;
 
 	while (guess != result) {
 		cout << "guess the result from " << a << " + " << b << " : ";
 		cin >> guess;
 
-		if (wrong == 3) {
+		if (wrong == 2) { // when 3 times wrong (wrong = 2), failed
 			cout << "wrong answer!" << endl;
-			lineBar();
-			cout << "you've failed : wrong answer 3 times." << endl;
+			cout << "\nyou've failed : wrong answer 3 times." << endl;
 			cout << "the correct answer is : " << result << endl;
 			break;
 		} else if (guess == result) {
-			lineBar();
 			cout << "your answer is correct, " << a << " + " << b << " = " << result << endl;
 			break;
 		} else {
 			cout << "wrong answer!" << endl;
-			wrong++;
+			wrong++; // wrong(0) + 1
 		}
 	}
 
