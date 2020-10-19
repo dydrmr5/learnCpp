4f9ddabf8d759d2afa0d356155b532e3f831eda4
Zulfikri Ramadhan Yoga <50226344+ctrlbzul5@users.noreply.github.com>
2020-10-19 21:04:06 +0700

    Create first.cpp

---
+++ cpp_basic/first.cpp
@@ -0,0 +1,11 @@
+#include <iostream> //congrats, this is your first line of code
+using namespace std;
+
+/*we must start a programming lesson with
+saying "hello" to the world. hahaha :D*/
+
+int main() {
+  cout << "Hello World! << endl; 
+  
+  return 0;
+}
