ce877c3c67af2268e2ae0e92d44748272ecef793
Zulfikri Ramadhan Yoga <50226344+ctrlbzul5@users.noreply.github.com>
2020-10-25 08:41:42 +0700

    Create day9_hackerrank.cpp

---
+++ cpp_hackerrank_30dayofcode/day9_hackerrank.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// My function
+int factorial(int n) {
+  if(n > 1) {
+    return n * factorial(n - 1);
+  } else {
+    return 1;
+  }
+}
+//
+
+int main() {
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    int result = factorial(n);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
