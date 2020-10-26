ef905fe0c0bc8bbf32892efe78e069f86de74f69
Zulfikri Ramadhan Yoga <50226344+ctrlbzul5@users.noreply.github.com>
2020-10-26 20:29:41 +0700

    Update and rename printing.cpp to 01_printing.cpp
    01 - Printing on C++

--- cpp_basic/01_printing.cpp
+++ cpp_basic/01_printing.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
+//using namespace std;
 
-int main()
-{
+int main() {
   // cout = console out
+	// if u use namespace std, don't need tu use std::,
+	// just type cout and endl
+	
   std::cout << "Zulfikri\n";
   std::cout << "Ramadhan\n";
   std::cout << "Yoga" << std::endl;
