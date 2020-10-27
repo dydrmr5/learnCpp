87b2f3084947d0051203be9d90a8534db8f50227
arfy slowy <40540262+slowy07@users.noreply.github.com>
2020-10-28 06:24:10 +0700

    warshall algorithm
    klo gkbsa ganti ```cpp #include <stdio.h>``` 
    
    ```cpp 
    void printSolution(int dist[][V])
    {
        printf ("The following matrix shows the shortest distances"
                " between every pair of vertices \n");
        for (int i = 0; i < V; i++)
        {
            for (int j = 0; j < V; j++)
            {
                if (dist[i][j] == INF)
                    printf("%7s", "INF");
                else
                    printf ("%7d", dist[i][j]);
            }
            printf("\n");
        }
    }
    ```

---
+++ warshall.cpp
@@ -0,0 +1,59 @@
+//warshall algorithm implementation 
+#include<cstdio>
+
+#define V 4 // Number of vertices in the graph
+#define INF 1e7
+
+void printSolution(int dist[][V]);
+void floydWarshall (int graph[][V])
+{
+    int dist[V][V], i, j, k;
+    for (i = 0; i < V; i++)
+        for (j = 0; j < V; j++)
+            dist[i][j] = graph[i][j];
+    for (k = 0; k < V; k++)
+    {
+        // Pick all vertices as source one by one
+        for (i = 0; i < V; i++)
+        {
+            for (j = 0; j < V; j++)
+            {
+                // If vertex k is on the shortest path from
+                // i to j, then update the value of dist[i][j]
+                if (dist[i][k] + dist[k][j] < dist[i][j])
+                    dist[i][j] = dist[i][k] + dist[k][j];
+            }
+        }
+    }
+    // Print the shortest distance matrix
+    printSolution(dist);
+}
+
+void printSolution(int dist[][V])
+{
+    printf ("The following matrix shows the shortest distances"
+            " between every pair of vertices \n");
+    for (int i = 0; i < V; i++)
+    {
+        for (int j = 0; j < V; j++)
+        {
+            if (dist[i][j] == INF)
+                printf("%7s", "INF");
+            else
+                printf ("%7d", dist[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+	  // Sample graph
+    int graph[V][V] = { {0,   5,  INF, 10},
+                        {INF, 0,   3, INF},
+                        {INF, INF, 0,   1},
+                        {INF, INF, INF, 0}
+                      };
+    floydWarshall(graph);
+    return 0;
+}
