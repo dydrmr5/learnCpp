9a48761fefe8547426ba05dbd90ef8ebb6ad540d
Zulfikri Ramadhan Yoga <50226344+ctrlbzul5@users.noreply.github.com>
2020-10-28 19:59:03 +0700

    Update pemasanganBaruPLN.cpp
    update 2 : better code

--- pemasanganBaruPLN.cpp
+++ pemasanganBaruPLN.cpp
@@ -1,27 +1,27 @@
 #include <iostream>
 #include <limits>
 #include <iomanip>
-using std::cout;
-using std::cin;
-using std::setw;
+using namespace std;
 
 void printMenu();
 void hargaSambunganR();
 void hargaSambunganI();
 
 int main() {
-	int harga, ppn, admin;
+	int harga, ppn, admin, total_biaya;
 	std::string sambungan;
 	char ulang, j_pelanggan, j_sambungan;
 	
 	system("clear"); //system("cls") on windows OS
 	
 	printMenu();
-	cout << "Pilih jenis pelanggan : "; cin >> j_pelanggan;
-	switch (j_pelanggan) {
-	case 'R':
+	cout << "Pilih jenis pelanggan (R/I) : "; cin >> j_pelanggan;
+
+	if (j_pelanggan == 'R') {
+		
 		hargaSambunganR();
-		cout << "Pilih jenis sambungan (1/2/3/4/5) : "; j_sambungan;
+		cin.ignore(numeric_limits<streamsize>::max(), '\n');
+		cout << "Pilih jenis sambungan (1/2/3/4/5) : "; cin >> j_sambungan;
 		switch (j_sambungan) {
 		case '1':
 			sambungan = "0-450 Watt\n";
@@ -47,10 +47,10 @@ int main() {
 		cout << "Jenis sambungan salah\n,pastikan input (1/2/3/4/5)!\n";
 			break;
 		}
-		break;
-	case 'I':
+	} else if (j_pelanggan == 'I') {
 		hargaSambunganI();
-		cout << "Pilih jenis sambungan (1/2/3/4/5) : "; j_sambungan;
+		cin.ignore(numeric_limits<streamsize>::max(), '\n');
+		cout << "Pilih jenis sambungan (1/2/3/4/5) : "; cin >> j_sambungan;
 		switch (j_sambungan) {
 		case '1':
 			sambungan = "4401-9500 Watt\n";
@@ -76,15 +76,23 @@ int main() {
 		cout << "Jenis sambungan salah\n,pastikan input (1/2/3/4/5)!\n";
 			break;
 		}
-		break;
-	default:
-	cout << "Pastikan input R atau I!\n";
-		break;
+	} else {
+		cout << "Pastikan input R atau I!\n";
 	}
 
 	//ppn dan administrasi
 	ppn = harga * 0.1;
 	admin = harga * 0.05;
+
+	cout << "\n== Detail Biaya Pemasangan Baru ==\n";
+	cout << "---------------------------------------------------\n";
+	cout << "Sambungan   : " << sambungan << "\n";
+	cout << "Harga       : Rp." << harga << "\n";
+	cout << "Pajak       : RP." << ppn << "(10% dari Harga)\n"; 
+	cout << "Admin       : Rp." << admin << "(5% dari Harga)\n";
+	cout << "---------------------------------------------------\n";
+	total_biaya = harga + ppn + admin;
+	cout << "Total Biaya : Rp." << total_biaya << "\n";
 
   return 0;
 }
@@ -98,17 +106,19 @@ void printMenu() { //print menu
 }
 
 void hargaSambunganR() { //harga sambungan : rumah tangga
-	cout << "(1) 0-450 Watt" << setw(16) << "Harga : Rp. 650000\n";
-	cout << "(2) 451-900 Watt" << setw(16) << "Harga : Rp. 850000\n";
-	cout << "(3) 901-1200 Watt" << setw(16) << "Harga : Rp. 1200000\n";
-	cout << "(4) 1201-2200 Watt" << setw(16) << "Harga : Rp. 1500000\n";
-	cout << "(5) 2201-4400 Watt" << setw(16) << "Harga : Rp. 1750000\n\n";
+	cout << "Jenis pelanggan : Rumah Tangga\n";
+	cout << "(1) 0-450 Watt" << "		(Rp. 650000)\n";
+	cout << "(2) 451-900 Watt" << "	(Rp. 850000)\n";
+	cout << "(3) 901-1200 Watt" << "	(Rp. 1200000)\n";
+	cout << "(4) 1201-2200 Watt" << "	(Rp. 1500000)\n";
+	cout << "(5) 2201-4400 Watt" << "	(Rp. 1750000)\n)\n";
 }
 
 void hargaSambunganI() { //harga sambungan : industri
-	cout << "(1) 4401-9500 Watt" << setw(16) << "Harga : Rp. 2250000\n";
-	cout << "(2) 9501-12000 Watt" << setw(16) << "Harga : Rp. 2750000\n";
-	cout << "(3) 12001-16000 Watt" << setw(16) << "Harga : Rp. 3250000\n";
-	cout << "(4) 16001-22000 Watt" << setw(16) << "Harga : Rp. 4500000\n";
-	cout << "(5) Di atas 22000 Watt" << setw(16) << "Harga : Rp. 6700000\n\n";
+	cout << "Jenis pelanggan : Industri\n";
+	cout << "(1) 4401-9500 Watt" << "	(Rp. 2250000)\n";
+	cout << "(2) 9501-12000 Watt" << "	(Rp. 2750000)\n";
+	cout << "(3) 12001-16000 Watt" << "	(Rp. 3250000)\n";
+	cout << "(4) 16001-22000 Watt" << "	(Rp. 4500000)\n";
+	cout << "(5) Di atas 22000 Watt" << "	(Rp. 6700000)\n)\n";
 }
