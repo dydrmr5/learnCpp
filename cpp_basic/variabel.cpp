16ff9ca5a13e9bb6c6bd4af8df7249e28c32c4f2
Zulfikri Ramadhan Yoga <50226344+ctrlbzul5@users.noreply.github.com>
2020-10-30 19:41:24 +0700

    Add files via upload

---
+++ cpp_basic/variabel.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int a = 1900; //variabel a dengan nilai 1900
+  int b = 99; //variabel b dengan nilai 99
+
+  cout << "Zulfikri lahir pada Tahun : "; cout << a + b << endl; //1900+99 = 1999
+  return 0;
+}
