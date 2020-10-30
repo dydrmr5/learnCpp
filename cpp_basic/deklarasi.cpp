6516284143d95815d84efcf8a0a6149fa3af16af
Zulfikri Ramadhan Yoga <50226344+ctrlbzul5@users.noreply.github.com>
2020-10-30 19:45:22 +0700

    Add files via upload

---
+++ cpp_basic/deklarasi.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int y;  //deklarasi variabel y
+  y = 51; //pemberian nilai variabel z
+  cout << y << endl; 
+
+  //penggunaan cin (console input/masukan)
+  int z; //deklarasi variabel z
+  cout << "masukkan nilai z: ";
+  cin >> z; //input nilai ke variabel z
+  cout << "nilai yang anda masukkan adalah: ";
+  cout << z << endl;
+
+  return 0;
+}
